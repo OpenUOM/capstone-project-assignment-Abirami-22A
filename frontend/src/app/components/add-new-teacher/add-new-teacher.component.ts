d113575d0a5a0304d46f26b319f9990f73c017a8
Abirami-22A <selvakumaranabirami@gmail.com>
2023-12-31 19:20:59 +0530

    frontend changes

--- frontend/src/app/components/add-new-teacher/add-new-teacher.component.ts
+++ frontend/src/app/components/add-new-teacher/add-new-teacher.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import {AppServiceService} from '../../app-service.service';
 import { Router } from '@angular/router';
 
